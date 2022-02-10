a19c3eab13cea9068214167a9297a488b6e02410
gyubum <sgb8170@naver.com>
2022-02-10 17:57:10 +0900

    영속성 관계 설정

--- shop/src/main/java/com/shop/repository/OrderRepository.java
+++ shop/src/main/java/com/shop/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.shop.repository;
 
-public class OrderRepository {
+import com.shop.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
