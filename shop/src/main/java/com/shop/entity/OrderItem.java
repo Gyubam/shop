dfbbfdbe3af26464d085328f80f807afc1ca2beb
gyubum <sgb8170@naver.com>
2022-02-22 03:33:18 +0900

    공통 속성 설정

--- shop/src/main/java/com/shop/entity/OrderItem.java
+++ shop/src/main/java/com/shop/entity/OrderItem.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity{
 
     @Id
     @GeneratedValue
@@ -26,8 +26,6 @@ public class OrderItem {
 
     private int orderPrice; // 주문 가격
     private int count; // 수량
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
 
 
 }
